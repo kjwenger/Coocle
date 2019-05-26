c3da205ce45e22a89616ed02749b8d8d81e44085
Klaus Wenger <kjwenger@yahoo.com>
2019-05-27 00:28:15 +0200

    added initial ionic code

--- phrotos/ionic/create.sh
+++ phrotos/ionic/create.sh
@@ -4,3 +4,7 @@ nvm install 10
 nvm use 10
 npm install -g cordova ionic
 ionic start CooclePhrotos tabs
+cd CooclePhrotos
+ionic cordova platform add ios
+ionic cordova build ios
+ionic cordova emulate ios
