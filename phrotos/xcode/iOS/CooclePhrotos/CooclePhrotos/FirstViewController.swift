bb71f707ad12e26f9e999d02db5118e6351cee47
Klaus Wenger <kjwenger@yahoo.com>
2019-05-26 23:08:28 +0200

    added initial xcode for iOS and macOS

---
+++ phrotos/xcode/iOS/CooclePhrotos/CooclePhrotos/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CooclePhrotos
+//
+//  Created by Klaus Wenger on 26.05.19.
+//  Copyright © 2019 Klaus Wenger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ phrotos/xcode/macOS/CooclePhrotos/CooclePhrotos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CooclePhrotos
+//
+//  Created by Klaus Wenger on 26.05.19.
+//  Copyright © 2019 Klaus Wenger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
